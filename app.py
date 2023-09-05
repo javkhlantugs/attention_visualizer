26e5f943744cdbef5bb2eb6aff9defb18c4d4d14
javkhlantugs <jay.mudgil@gmail.com>
2023-09-05 16:14:24 -0400

    all 12 visuals present under layer dropdown menus

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-from flask import Flask, render_template, request, session
-from mask import *
+from flask import Flask, request, render_template, session
 import os
 import uuid
+from mask import *
 
 app = Flask(__name__)
-app.secret_key = 'habibi'
+app.secret_key = os.urandom(24).hex()  # Set your secret key for session management
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    session['new_session_id'] = str(uuid.uuid4())
-    if request.method == 'POST':
-        sentence = request.form['sentence']
-        session['img_path'] = os.path.join("static", "img", session['new_session_id'])
-        os.makedirs(session['img_path'])
-        generated_sentences = generate(sentence, session['new_session_id'])
-        return render_template('index.html', sentence=sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
+@app.before_request
+def before_request():
+    if 'new_session_id' not in session:
+        session['new_session_id'] = str(uuid.uuid4())
 
+@app.route('/', methods=['GET'])
+def index_get():
     return render_template('index.html', sentence=None, generated_sentences=None, attention_diagrams=None)
 
-# @app.teardown_request
-# def delete_session_folder(exception=None):
-#     folder_path = session.get('img_path')
-#     if folder_path:
-#         import shutil
-#         shutil.rmtree(folder_path, ignore_errors=True)
+@app.route('/', methods=['POST'])
+def index_post():
+    sentence = request.form['sentence']
+    session['img_path'] = os.path.join("static", "img", session['new_session_id'])
+    if not os.path.exists(session['img_path']):
+        os.makedirs(session['img_path'])
+    generated_sentences = generate(sentence, session['new_session_id'])
+    return render_template('result.html', sentence=sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
 
 if __name__ == '__main__':
     app.run(debug=True)
