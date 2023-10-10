bd048d624182221c1c4a3cf96963096d5cb8c0f1
javkhlantugs <jay.mudgil@gmail.com>
2023-10-10 18:30:48 -0400

    added javascript for user experience

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, jsonify, redirect, url_for, render_template, session
 import os
 import uuid
 from mask import *
@@ -20,14 +20,24 @@ def before_request():
 def index_get():
     return render_template('index.html', sentence=None, generated_sentences=None, attention_diagrams=None)
 
-@app.route('/', methods=['POST'])
-def index_post():
-    sentence = request.form['sentence']
+
+@app.route('/api', methods=['POST'])
+def receive_updated_sentence():
+    # Receive the JSON data from the request
+    data = request.get_json()
+
+    # Access the updated sentence from the JSON data
+    updated_sentence = data.get('updatedSentence')
+
+    # Process the updated sentence as needed
+    # For example, you can save it to a session variable or do any other processing
+
+    # Render the 'result.html' template with the processed data
     session['img_path'] = os.path.join("static", "img", session['new_session_id'])
     if not os.path.exists(session['img_path']):
         os.makedirs(session['img_path'])
-    generated_sentences = generate(sentence, session['new_session_id'])
-    return render_template('result.html', sentence=sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
+    generated_sentences = generate(updated_sentence, session['new_session_id'])
+    return render_template('result.html', sentence=updated_sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
 
 if __name__ == '__main__':
     app.run(debug=True)
