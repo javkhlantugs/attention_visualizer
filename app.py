231933c8bc597bda7af58e204f2dde66e866bcec
javkhlantugs <jay.mudgil@gmail.com>
2023-10-11 20:48:44 -0400

    completed redirection to /result page using javascript line

--- app.py
+++ app.py
@@ -20,24 +20,32 @@ def before_request():
 def index_get():
     return render_template('index.html', sentence=None, generated_sentences=None, attention_diagrams=None)
 
-
 @app.route('/api', methods=['POST'])
-def receive_updated_sentence():
+def generate_sentence():
     # Receive the JSON data from the request
     data = request.get_json()
 
     # Access the updated sentence from the JSON data
     updated_sentence = data.get('updatedSentence')
-
+    session['updated_sentence'] = updated_sentence
     # Process the updated sentence as needed
     # For example, you can save it to a session variable or do any other processing
 
-    # Render the 'result.html' template with the processed data
+    # Redirect to the '/result' route without query parameters
+    return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    # Process the sentence as needed
+    # For example, you can retrieve the sentence from the session variable
+    sentence = session['updated_sentence']
+
     session['img_path'] = os.path.join("static", "img", session['new_session_id'])
     if not os.path.exists(session['img_path']):
         os.makedirs(session['img_path'])
-    generated_sentences = generate(updated_sentence, session['new_session_id'])
-    return render_template('result.html', sentence=updated_sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
+    generated_sentences = generate(sentence, session['new_session_id'])
+    return render_template('result.html', sentence=sentence, generated_sentences=generated_sentences, attention_diagrams=session["img_path"])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
